1db12db65a84d9d3836eb630f8b32f592db72ba0
TheoToni <32997269+TheoToni@users.noreply.github.com>
2023-06-12 23:21:24 +0200

    adding hamburger menu

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,15 +2,20 @@ import { motion } from "framer-motion";
 import { useState } from "react";
 
 function Nav() {
+  const [toggled, setToggled] = useState(false);
   return (
     <main>
-      <nav className="bg-[#202020] flex items-center h-24 pl-5 pr-5">
+      <nav className=" flex items-center justify-between h-24 pl-5 pr-5">
         <div>
-          <h1 className="text-white text-sm font-bold">Resume</h1>
-          <h2 className="text-white text-lg font-bold">Antoni Theodorou</h2>
+          <h1 className="text-black text-sm font-bold">Resume</h1>
+          <h2 className="text-black text-lg font-bold">Antoni Theodorou</h2>
         </div>
 
-        <div></div>
+        <div className="space-y-1">
+          <span className="block h-1 w-8 bg-black"></span>
+          <span className="block h-1 w-8 bg-black"></span>
+          <span className="block h-1 w-8 bg-black"></span>
+        </div>
       </nav>
     </main>
   );
