f4f37d83d15143b1445efba25ee20f9e194fdafa
TheoToni <32997269+TheoToni@users.noreply.github.com>
2023-06-12 22:58:45 +0200

    working on nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,14 @@
 function Nav() {
-  return <div>Nav</div>;
+  return (
+    <main>
+      <nav className="bg-[#333] flex items-center h-24">
+        <div>
+          <h1 className="text-white text-sm font-bold  ">Resume</h1>
+          <h2 className="text-white text-lg font-bold  ">Antoni Theodorou</h2>
+        </div>
+      </nav>
+    </main>
+  );
 }
 
 export default Nav;
