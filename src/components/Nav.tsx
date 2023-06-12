44f663caa67b0a8f7cdbf187b85a3fd00183d61c
TheoToni <32997269+TheoToni@users.noreply.github.com>
2023-06-12 23:04:43 +0200

    nav progress

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,10 @@
 function Nav() {
   return (
     <main>
-      <nav className="bg-[#333] flex items-center h-24">
+      <nav className="bg-[#202020] flex items-center h-24 pl-5 pr-5">
         <div>
-          <h1 className="text-white text-sm font-bold  ">Resume</h1>
-          <h2 className="text-white text-lg font-bold  ">Antoni Theodorou</h2>
+          <h1 className="text-white text-sm font-bold">Resume</h1>
+          <h2 className="text-white text-lg font-bold">Antoni Theodorou</h2>
         </div>
       </nav>
     </main>
